1e911bc77d80010094d2f85a9e7e957749ce4fe8
takkan <taku.kaneda@readspeaker.com>
2020-11-04 07:10:15 +0100

    add default_settings file

---
+++ default_settings.py
@@ -0,0 +1,11 @@
+import os
+import pandas as pd
+
+
+## direcotries.
+home_dir = r'/home/takkan'
+audio_corpora_dir = os.path.join(home_dir, 'audio_corpora/exercise/wav')
+# used to import other repositories.
+# all related repos are expected to be under this directory.
+repos_dir = os.path.join(home_dir, 'repos')
+
