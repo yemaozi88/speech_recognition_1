afdc21836fb1042241011431eff1b044c31391ac
Aki <aki.kunikoshi@readspeaker.com>
2020-11-12 12:58:40 +0100

    regression exercise is added.

--- default_settings.py
+++ default_settings.py
@@ -5,6 +5,8 @@ import pandas as pd
 ## direcotries.
 home_dir = r'/home/takkan'
 audio_corpora_dir = os.path.join(home_dir, 'data/exercise')
+
 # used to import other repositories.
 # all related repos are expected to be under this directory.
 repos_dir = os.path.join(home_dir, 'repos')
+
