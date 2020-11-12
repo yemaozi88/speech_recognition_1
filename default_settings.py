60cbc4387c73b1e1851a91d76d3c831173beec5f
takkan <taku.kaneda@readspeaker.com>
2020-11-12 04:38:17 +0100

    add audio dir

--- default_settings.py
+++ default_settings.py
@@ -4,8 +4,7 @@ import pandas as pd
 
 ## direcotries.
 home_dir = r'/home/takkan'
-audio_corpora_dir = os.path.join(home_dir, 'audio_corpora/exercise/wav')
+audio_corpora_dir = os.path.join(home_dir, 'data/exercise')
 # used to import other repositories.
 # all related repos are expected to be under this directory.
 repos_dir = os.path.join(home_dir, 'repos')
-
