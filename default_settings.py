6f936589f0195fc0ac32fc3e882698d7bd4b2d5b
taku <taku.kaneda@readspeaker.com>
2021-05-07 08:33:13 +0200

    Vowel classification

--- default_settings.py
+++ default_settings.py
@@ -4,7 +4,10 @@ import os
 
 ## direcotries.
 home_dir = r'/home/takkan'
-audio_corpora_dir = os.path.join(home_dir, 'data/exercise')
+audio_corpora_dir = os.path.join(home_dir, 'experiments/taku_voice/wavs')
+digit_dir = os.path.join(audio_corpora_dir, 'digit_trimmed')
+vowel_dir = os.path.join(audio_corpora_dir, 'vowel_trimmed')
+features_dir = os.path.join(home_dir, 'experiments/taku_voice/features')
 
 # used to import other repositories.
 # all related repos are expected to be under this directory.
