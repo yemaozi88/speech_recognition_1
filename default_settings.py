4870ddbff8fd795ef502b6175c65508a1e76f1f4
taku <taku.kaneda@readspeaker.com>
2021-07-21 09:00:51 +0200

    Add a notebook of mfcc distance

--- default_settings.py
+++ default_settings.py
@@ -6,10 +6,11 @@ import os
 home_dir = r'/home/takkan'
 audio_corpora_dir = os.path.join(home_dir, 'experiments/taku_voice/wavs')
 digit_dir = os.path.join(audio_corpora_dir, 'digit_trimmed')
+jpn_source_digit_dir = os.path.join(audio_corpora_dir, 'jpn_digit_3times-1')
+jpn_target_digit_dir = os.path.join(audio_corpora_dir, 'jpn_digit_3times-2')
 vowel_dir = os.path.join(audio_corpora_dir, 'vowel_trimmed')
 features_dir = os.path.join(home_dir, 'experiments/taku_voice/features')
 
 # used to import other repositories.
 # all related repos are expected to be under this directory.
 repos_dir = os.path.join(home_dir, 'repos')
-
